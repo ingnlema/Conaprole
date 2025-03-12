7ee2cd271becaefea94cd78ecc52e5d9110ae1e5
ingnlema <ing.nlema@gmail.com>
2025-03-11 23:02:04 -0300

    fix dockerfile

--- src/Conaprole.Orders.Infrastructure/obj/Debug/net8.0/Conaprole.Orders.Infrastructure.AssemblyInfo.cs
+++ src/Conaprole.Orders.Infrastructure/obj/Debug/net8.0/Conaprole.Orders.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conaprole.Orders.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e7c6464fd440092e55defaee004f81e3e69649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d89165b305c45fcaf6b5f307d771f2d7b8c162")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conaprole.Orders.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conaprole.Orders.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
